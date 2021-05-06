2597a978447907d2d3ff7f46f3dd7dfeac47c80c
Marino Missiroli <m.missiroli@cern.ch>
2021-05-06 16:28:24 +0200

    [NTupleAnalysis] updated test/

--- NTupleAnalysis/test/runAnalysis_hltPhase2.sh
+++ NTupleAnalysis/test/runAnalysis_hltPhase2.sh
@@ -3,12 +3,14 @@
 # directory with input JMETriggerNTuple(s)
 #INPDIR=/eos/cms/store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/Upgrade/JetMET_PhaseII/JMETriggerAnalysis_phase2/ntuples/output_hltPhase2_201209
 #INPDIR=/eos/cms/store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/Upgrade/JetMET_PhaseII/JMETriggerAnalysis_phase2/ntuples/output_hltPhase2_210421_HFtest
-INPDIR=/eos/cms/store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/Upgrade/JetMET_PhaseII/JMETriggerAnalysis_phase2/ntuples/output_hltPhase2_210427_HFtestCalo
+#INPDIR=/eos/cms/store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/Upgrade/JetMET_PhaseII/JMETriggerAnalysis_phase2/ntuples/output_hltPhase2_210427_HFtestCalo
+INPDIR=/eos/cms/store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/Upgrade/JetMET_PhaseII/JMETriggerAnalysis_phase2/ntuples/output_hltPhase2_210428_hltMuonVal
 
 # directory with outputs of NTupleAnalysis
 #OUTDIR=output_hltPhase2_201209_HLTTDR_hltGENdR05
 #OUTDIR=output_hltPhase2_210421_HFtest_hltPt0dR06
-OUTDIR=output_hltPhase2_210427_HFtestCalo
+#OUTDIR=output_hltPhase2_210427_HFtestCalo
+OUTDIR=output_hltPhase2_210428_hltMuonVal
 
 mkdir -p ${OUTDIR}
 [ -d ${OUTDIR}/ntuples ] || (ln -sf ${INPDIR} ${OUTDIR}/ntuples)
@@ -23,11 +25,13 @@ merge_batchOutputs.py -l 1 -i ${OUTDIR}/jobs/*/*.root -o ${OUTDIR}/outputs
 rm -rf ${OUTDIR}/jobs
 
 NUM_PROC=$(nproc)
-if [[ ${HOSTNAME} == lxplus* ]]; then NUM_PROC=1; fi;
-
 for rootfile_i in ${OUTDIR}/outputs/*/*.root; do
-  while [ $(jobs -p | wc -l) -ge ${NUM_PROC} ]; do sleep 5; done
-  jmeAnalysisHarvester.py -l 1 -i ${rootfile_i} -o ${OUTDIR}/harvesting || true &
+  if [[ ${HOSTNAME} == lxplus* ]]; then
+    jmeAnalysisHarvester.py -l 1 -i ${rootfile_i} -o ${OUTDIR}/harvesting
+  else
+    while [ $(jobs -p | wc -l) -ge ${NUM_PROC} ]; do sleep 5; done
+    jmeAnalysisHarvester.py -l 1 -i ${rootfile_i} -o ${OUTDIR}/harvesting || true &
+  fi
 done
 unset rootfile_i
 
@@ -36,4 +40,4 @@ wait || true
 
 rm -rf ${OUTDIR}/outputs
 
-./plot_hltPhase2TDR.py -i ${OUTDIR}/harvesting -o ${OUTDIR}_plots
+#./plot_hltPhase2TDR.py -i ${OUTDIR}/harvesting -o ${OUTDIR}_plots
