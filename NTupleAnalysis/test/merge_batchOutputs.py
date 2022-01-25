e63512c08b9991e0554899edcd06e0b57bc35aff
pallabidas <pallabi91@gmail.com>
2022-01-25 17:02:54 +0100

    updated scripts for validation studies, Dec 2021

--- NTupleAnalysis/test/merge_batchOutputs.py
+++ NTupleAnalysis/test/merge_batchOutputs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """merge outputs of batch jobs"""
 from __future__ import print_function
 import os
--- NTupleAnalysis/test/runAnalysis_hltRun3.sh
+++ NTupleAnalysis/test/runAnalysis_hltRun3.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
+source env.sh
 
 # directory with input JMETriggerNTuple(s)
-INPDIR=/eos/cms/store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/Upgrade/JetMET_PhaseII/JMETriggerAnalysis_run3/ntuples/hltRun3_testTRK_210331
+INPDIR=/eos/cms/store/group/phys_jetmet/pdas/20Oct_ntuples/Calibs_v2/Run3TRK
 
 # directory with outputs of NTupleAnalysis
-OUTDIR=output_hltRun3_testTRK_210331_v03
+OUTDIR=Run3TRK
+OUTPUTDIR=/eos/cms/store/group/phys_jetmet/pdas/Calibs_v2/Run3TRK/
 
-mkdir -p ${OUTDIR}
-[ -d ${OUTDIR}/ntuples ] || (ln -sf ${INPDIR} ${OUTDIR}/ntuples)
+#mkdir -p ${OUTDIR}
+#[ -d ${OUTDIR}/ntuples ] || (ln -sf ${INPDIR} ${OUTDIR}/ntuples)
 
-batch_driver.py -l 1 -n 50000 -p JMETriggerAnalysisDriverRun3 \
- -i ${OUTDIR}/ntuples/*/*.root -o ${OUTDIR}/jobs \
- --AccountingGroup group_u_CMS.CAF.PHYS --JobFlavour longlunch
-
-batch_monitor.py -i ${OUTDIR}/jobs -r --repe -f 900
-
-merge_batchOutputs.py -l 1 -i ${OUTDIR}/jobs/*/*.root -o ${OUTDIR}/outputs
-rm -rf ${OUTDIR}/jobs
-
-NUM_PROC=$(nproc)
-if [[ ${HOSTNAME} == lxplus* ]]; then NUM_PROC=3; fi;
-for rootfile_i in ${OUTDIR}/outputs/*/*.root; do
-  while [ $(jobs -p | wc -l) -ge ${NUM_PROC} ]; do sleep 5; done
-  jmeAnalysisHarvester.py -l 1 -i ${rootfile_i} -o ${OUTDIR}/harvesting || true &
-done
-unset rootfile_i
-
-jobs
-wait || true
-
-rm -rf ${OUTDIR}/outputs
+#batch_driver.py -l 1 -n 5000 -p JMETriggerAnalysisDriverRun3 \
+# -i ${OUTDIR}/ntuples/*.root -o ${OUTDIR}/jobs \
+# -od ${OUTPUTDIR} \
+# --JobFlavour longlunch
+#
+#batch_monitor.py -i ${OUTDIR}/jobs -r --repe -f 900
+#
+#merge_batchOutputs.py -l 1 -i ${OUTPUTDIR}/*.root -o ${OUTPUTDIR}/outputs
+#rm -rf ${OUTDIR}/jobs
+#
+#NUM_PROC=$(nproc)
+#if [[ ${HOSTNAME} == lxplus* ]]; then NUM_PROC=3; fi;
+#for rootfile_i in ${OUTPUTDIR}/outputs/*/*.root; do
+#  while [ $(jobs -p | wc -l) -ge ${NUM_PROC} ]; do sleep 5; done
+#  jmeAnalysisHarvester.py -l 1 -i ${rootfile_i} -o ${OUTPUTDIR}/harvesting || true &
+#done
+#unset rootfile_i
+#
+#jobs
+#wait || true
+#
+#rm -rf ${OUTDIR}/outputs
