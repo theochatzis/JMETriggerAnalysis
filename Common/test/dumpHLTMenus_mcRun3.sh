43947e2a17611e89cb2b352adcfdcdd15a5ebb64
Theo Chatzistavrou <teophysix@gmail.com>
2024-09-20 17:39:19 +0200

    shell script for menu production

--- Common/test/dumpHLTMenus_mcRun3.sh
+++ Common/test/dumpHLTMenus_mcRun3.sh
@@ -1,22 +1,18 @@
 #!/bin/bash -e
 
-hltGetConfiguration /dev/CMSSW_13_0_0/GRun \
-   --globaltag 126X_mcRun3_2023_forPU65_v3 \
-   --full \
-   --offline \
+hltGetConfiguration /dev/CMSSW_14_0_0/GRun \
+   --globaltag auto:phase1_2024_realistic \
    --mc \
    --unprescale \
-   --process HLTX \
    --output minimal \
    --max-events 10 \
-   --input /store/mc/Run3Winter23Digi/TT_TuneCP5_13p6TeV_powheg-pythia8/GEN-SIM-RAW/126X_mcRun3_2023_forPU65_v1_ext1-v2/40002/cbcb2b23-174a-4e7f-a385-152d9c5c5b87.root \
-   --eras Run3 --l1-emulator FullMC --l1 L1Menu_Collisions2023_v1_0_0_xml \
-   --path MC_*,HLTriggerF*,Status*,HLT_PFJet60_v*,HLT_PFJet140_v*,HLT_PFJet320_v*,HLT_PFJet500_v*,HLT_PFHT780_v*,HLT_PFHT890_v*,HLT_PFHT1050_v*,HLT_PFMET120_PFMHT120_IDTight_v*,HLT_PFMET140_PFMHT140_IDTight_v*,HLT_PFMETTypeOne140_PFMHT140_IDTight_v* \
-   --customise HLTrigger/Configuration/customizeHLTFor2023.customizeHLTFor2023_v3_fromCondDb \
+   --input /store/mc/Run3Winter24Digi/TT_TuneCP5_13p6TeV_powheg-pythia8/GEN-SIM-RAW/133X_mcRun3_2024_realistic_v8-v2/80000/dc984f7f-2e54-48c4-8950-5daa848b6db9.root \
+   --eras Run3_2024 --l1-emulator uGT --l1 L1Menu_Collisions2024_v1_3_0_xml \
+   --path MC_*,HLTriggerF*,Status*,HLT_PFJet60_v*,HLT_PFJet140_v*,HLT_PFJet320_v*,HLT_PFJet500_v*,HLT_PFHT780_v*,HLT_PFHT890_v*,HLT_PFHT1050_v*,HLT_PFMET*_PFMHT*_IDTight_v* \
    > tmp.py
 
 # dump configuration 
-edmConfigDump tmp.py > "${CMSSW_BASE}"/src/JMETriggerAnalysis/Common/python/configs/HLT_dev_CMSSW_13_0_0_GRun_configDump.py
+edmConfigDump tmp.py > "${CMSSW_BASE}"/src/JMETriggerAnalysis/Common/python/configs/HLT_dev_CMSSW_14_0_0_GRun_configDump.py
 
 #test running it
 #cmsRun tmp.py &> test.log
