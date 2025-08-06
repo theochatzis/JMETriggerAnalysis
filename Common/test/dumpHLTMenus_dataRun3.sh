3e4bd4b094ad4876c85aa4e9f99a156ebc23f972
Theo Chatzistavrou <teophysix@gmail.com>
2025-08-06 13:50:05 +0200

    data menu

--- Common/test/dumpHLTMenus_dataRun3.sh
+++ Common/test/dumpHLTMenus_dataRun3.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -e
 
 # Taken from SWGuideGlobalHLT page : https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideGlobalHLT
-
-hltGetConfiguration /dev/CMSSW_14_0_0/GRun \
-   --globaltag 140X_dataRun3_HLT_for2024TSGStudies_v1 \
+hltGetConfiguration /dev/CMSSW_15_0_0/GRun \
+   --globaltag 150X_dataRun3_HLT_v1 \
    --data \
    --unprescale \
    --output minimal \
-   --max-events 10 \
-   --eras Run3 --l1-emulator uGT --l1 L1Menu_Collisions2024_v1_2_0_xml \
-   --input /store/data/Run2023D/EphemeralHLTPhysics0/RAW/v1/000/370/293/00000/2ef73d2a-1fb7-4dac-9961-149525f9e887.root \
+   --max-events 100 \
+   --eras Run3_2025 --l1-emulator uGT --l1 L1Menu_Collisions2025_v1_2_0_xml \
+   --input /store/data/Run2025C/EphemeralHLTPhysics3/RAW/v1/000/392/250/00000/745d08f7-3834-4757-8f58-5294a13f13ae.root \
    > tmp.py
 
 # dump configuration 
-edmConfigDump tmp.py > "${CMSSW_BASE}"/src/JMETriggerAnalysis/Common/python/configs/HLT_dev_CMSSW_14_0_0_GRun_configDump_data.py
+edmConfigDump tmp.py > "${CMSSW_BASE}"/src/JMETriggerAnalysis/Common/python/configs/HLT_dev_CMSSW_15_0_0_GRun_configDump_data.py
 
 #test running it
 #cmsRun tmp.py &> test.log
