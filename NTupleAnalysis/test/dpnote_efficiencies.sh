21ecba0059efde1ba26f17335f5f7715fb1af22f
Theo Chatzistavrou <teophysix@gmail.com>
2025-08-12 12:58:12 +0200

    dp note exe

---
+++ NTupleAnalysis/test/dpnote_efficiencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 plotEfficienciesDPNote.py \
+  --input /eos/user/t/tchatzis/DPNoteSubmitter/ \
+  --merged_file /eos/user/t/tchatzis/DPNoteSubmitter/merged.root \
+  --config ./plotsEfficiencies_configs/dpnote_config.yaml \
+  --output_dir /eos/user/t/tchatzis/DPNotePlots
